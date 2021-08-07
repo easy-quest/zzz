7720f3ffbd4b29e7211d4f187f86d8c83eeb58af
Ivan Yastrebov <easy-quest@mail.ru>
2021-08-07 18:44:01 +0300

    изменено:      README 	изменено:      zzz.py

--- zzz.py
+++ zzz.py
@@ -4,5 +4,6 @@ click.clear()
 @click.command()
 def up():
     """Обновление системы."""
-    click.secho('EASYQQUEST!',bg='yellow',fg='black')
-    click.launch('help')
+    click.secho('EASYQUEST!',bg='yellow',fg='black')
+    click.pause()
+    click.launch('http://easyquest.host/')
